9946a3454acb486c6f63ccc9b8adaaca6deb5b91
yangjg <lgzj2011@gmail.com>
2017-02-25 00:08:31 +0800

    rebuild my blog with Jekyll instand of hexo

--- js/hux-blog.js
+++ js/hux-blog.js
@@ -17,21 +17,21 @@ $(function() {
  * so I modify the _layout/post and CSS to make post-img responsive!
  */
 // $(function() {
-// 	$("img").addClass("img-responsive");
+//  $("img").addClass("img-responsive");
 // });
 
 // responsive tables
 $(document).ready(function() {
-	$("table").wrap("<div class='table-responsive'></div>");
-	$("table").addClass("table");
+    $("table").wrap("<div class='table-responsive'></div>");
+    $("table").addClass("table");
 });
 
 // responsive embed videos
 $(document).ready(function () { 
     $('iframe[src*="youtube.com"]').wrap('<div class="embed-responsive embed-responsive-16by9"></div>');
-	$('iframe[src*="youtube.com"]').addClass('embed-responsive-item');
+    $('iframe[src*="youtube.com"]').addClass('embed-responsive-item');
     $('iframe[src*="vimeo.com"]').wrap('<div class="embed-responsive embed-responsive-16by9"></div>');
-	$('iframe[src*="vimeo.com"]').addClass('embed-responsive-item');
+    $('iframe[src*="vimeo.com"]').addClass('embed-responsive-item');
 });
 
 // Navigation Scripts to Show Header on Scroll-Up
